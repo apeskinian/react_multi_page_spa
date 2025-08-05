a9eb4ca23665a0d80aa43ec3bfb982aa372e178a
Matthew Aitken <matt@itken.net>
2025-08-05 14:20:51 +0100

    Use Link to navigate between pages

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,15 @@
+// Using Link element instead of anchor elements so that a new request is not sent
+import { Link } from "react-router-dom";
+
 function Homepage() {
-    return <h1>My Home Page!</h1>
+    return (
+        <>
+            <h1>My Home Page!</h1>
+            <p>
+                Go to the list of <Link to="/products">products</Link>.
+            </p>
+        </>
+    )
 }
 
 export default Homepage;
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 function ProductsPage() {
-    return <h1>The Products Page</h1>
+    return (
+        <>
+            <h1>The Products Page</h1>
+            <p>
+                Go back <Link to="/">home</Link>.
+            </p>
+        </>
+    )
 }
 
 export default ProductsPage;
