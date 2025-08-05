3c742d24c90fa2dd90653a2039bfe99adb86ad4d
Matthew Aitken <matt@itken.net>
2025-08-05 14:57:54 +0100

    Use useNavigate for example

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,22 @@
 // Using Link element instead of anchor elements so that a new request is not sent
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Homepage() {
+    // Using useNavigate to programmatically navigate to pages
+    const navigate = useNavigate();
+
+    function navigateHandler() {
+        navigate('/products');
+    }
+
     return (
         <>
             <h1>My Home Page!</h1>
             <p>
                 Go to the list of <Link to="/products">products</Link>.
+            </p>
+            <p>
+                <button onClick={navigateHandler}>Navigate</button>
             </p>
         </>
     )
