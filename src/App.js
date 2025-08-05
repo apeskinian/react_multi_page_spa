a8287b11030611d7a675c81deb688933712672a7
Matthew Aitken <matt@itken.net>
2025-08-05 15:14:53 +0100

    Add dynamic path links

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Homepage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RouteLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetails";
 
 // Alternative method for creating route objects:
 // const routeDefinitions = createRoutesFromElements(
@@ -25,7 +26,8 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { path: '/', element: <Homepage /> },
-      { path: '/products', element: <ProductsPage /> }
+      { path: '/products', element: <ProductsPage /> },
+      { path: '/products/:productId', element: <ProductDetailPage /> }
     ],
   },
 ]);
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,5 +1,11 @@
 import { Link } from "react-router-dom";
 
+const PRODUCTS = [
+    { id: 'p1', title: 'Product 1' },
+    { id: 'p2', title: 'Product 2' },
+    { id: 'p3', title: 'Product 3' },
+];
+
 function ProductsPage() {
     return (
         <>
@@ -7,6 +13,11 @@ function ProductsPage() {
             <p>
                 Go back <Link to="/">home</Link>.
             </p>
+            <ul>
+                {PRODUCTS.map((prod) => (
+                    <li key={prod.id}><Link to={`/products/${prod.id}`}>{prod.title}</Link></li>
+                ))}
+            </ul>
         </>
     )
 }
